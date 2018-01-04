be2ecf6c00e371383f2423c7f33827aa1d3631d1
郝小永 <32768756+haoxiaoyong1014@users.noreply.github.com>
2018-01-04 11:04:34 +0800

    Update Swagger2Configuration.java

--- src/main/java/example/config/Swagger2Configuration.java
+++ src/main/java/example/config/Swagger2Configuration.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * Created by 李恒名 on 2016/10/12.
+ * Created by hxy
  */
 @Configuration
 @EnableSwagger2
